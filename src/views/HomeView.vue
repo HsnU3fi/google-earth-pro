e7bfdff36f14fce596498614a1381f02b95f1614
Hsn <hsn@pop-os.localdomain>
2023-09-09 16:26:26 +0330

    material design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,34 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<h4>cnjksdnckjn</h4>
+  <Deckgl />/
 </template>
+
+<script>
+
+// Components
+import DeckGl from '../components/deckgl/DeckGl.vue';
+
+export default {
+  name: 'HomeView',
+
+  components: {
+    DeckGl,
+  },
+};
+</script>
+<!--<template>-->
+<!--  <div id="app">-->
+<!--    <DeckGL />-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--import DeckGL from '@hirelofty/vue_deckgl'-->
+
+<!--export default {-->
+<!--  name: 'App',-->
+<!--  components: {-->
+<!--    DeckGL-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
